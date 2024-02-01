38717ed57ca27af667a1ce5df19d91ec25177996
Adithya Ajith <adithyajith925@gmail.com>
2024-02-01 11:02:04 -0600

    Apply full game functionality to front-end

--- ttt.js
+++ ttt.js
@@ -1,14 +1,12 @@
 function createBoard() {
     let board = [null, null, null, null, null, null, null, null, null];
 
-    const canPlayPiece = (row, col) => {
-        if (row > 3 || row < 1) return false;
-        if (col > 3 || col < 1) return false;
-        return board[(row - 1) * 3 + col - 1] === null;
+    const canPlayPiece = (index) => {
+        if (index < 0 || index > 8) return false;
+        return board[index] === null;
     }
 
-    const playPiece = (player, row, col) => {
-        let index = (row - 1) * 3 + col - 1;
+    const playPiece = (player, index) => {
         board[index] = player;
         return;
     }
@@ -50,7 +48,11 @@ function createBoard() {
             ${board[6]} ${board[7]} ${board[8]}`;
     }
 
-    return { canPlayPiece, playPiece, checkBoard, getBoardState };
+    const resetBoard = () => {
+        board = [null, null, null, null, null, null, null, null, null];
+    }
+
+    return { canPlayPiece, playPiece, checkBoard, getBoardState, resetBoard };
 }
 
 function createPlayer(name, symbol) {
@@ -76,42 +78,100 @@ function createGame() {
 
     const gameBoard = createBoard();
 
-    const takeTurn = (row, col) => {
+    const canTakeTurn = (index) => {
+        return gameBoard.canPlayPiece(index);
+    }
+
+    const takeTurn = (index) => {
         // Put the piece into position
-        if (gameBoard.canPlayPiece(row, col)) {
-            gameBoard.playPiece(currentPlayer.getSymbol(), row, col);
-        }
+        gameBoard.playPiece(currentPlayer.getSymbol(), index);
 
         // Check if game has ended
         let gameStatus = gameBoard.checkBoard();
         if (gameStatus !== null) {
             return gameStatus;
         }
+        return null;
+    } 
 
-        // Swap turn
+    const swapTurn = () => {
         if (currentPlayer === player1) {
             currentPlayer = player2;
         }
         else {
             currentPlayer = player1;
         }
-
-        return null;
-    } 
+    }
 
     const getCurrentPlayer = () => {
         return currentPlayer;
+    }
+
+    const resetGame = () => {
+        currentPlayer = player1;
+        gameBoard.resetBoard();
     }
 
     const printBoardState = () => {
         console.log(gameBoard.getBoardState());
     }
 
-    return { takeTurn, getCurrentPlayer, printBoardState };
+    return { canTakeTurn, takeTurn, getCurrentPlayer, swapTurn, printBoardState, resetGame };
 }
 
 function displayHandler() {
-    
+    const mainGame = createGame();
+    const gameButtons = document.querySelectorAll('.board-square');
+    const statusMessage = document.querySelector('.game-status h4');
+    const resetButton = document.querySelector('#reset-game');
+
+    resetButton.addEventListener('click', (e) => {
+        resetGame();
+    });
+
+    const resetGame = () => {
+        mainGame.resetGame();
+        gameButtons.forEach(element => {
+            element.textContent = "";
+            element.classList.add("clickable");
+            element.addEventListener('click', (e) => {
+                takeTurn(e.target);
+            })
+        });
+        statusMessage.textContent = `It is ${mainGame.getCurrentPlayer().getSymbol()}'s turn`
+    }
+
+    const takeTurn = (square) => {
+        let index = square.dataset.index;
+        if (mainGame.canTakeTurn(index - 1)) {
+            let result = mainGame.takeTurn(index - 1);
+            square.textContent = mainGame.getCurrentPlayer().getSymbol();
+            if (result !== null) {
+                handleGameEnd(result);
+                return;
+            }
+            mainGame.swapTurn();
+            statusMessage.textContent = `It is ${mainGame.getCurrentPlayer().getSymbol()}'s turn`
+            square.classList.remove("clickable");
+            square.removeEventListener('click', takeTurn);
+        }
+    }
+
+    const handleGameEnd = (result) => {
+        if (result === "Draw") {
+            statusMessage.textContent = "The game is a draw!";
+        }
+        else {
+            statusMessage.textContent = `${mainGame.getCurrentPlayer().getName()} (${mainGame.getCurrentPlayer().getSymbol()}) has won!`;
+        }
+        gameButtons.forEach(element => {
+            element.classList.remove("clickable");
+            element.removeEventListener('click', takeTurn);
+        });
+        
+    }
+
+    resetGame();
 }
 
-let mainGame = createGame();
+let start = displayHandler();
